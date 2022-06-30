2e8c85fe018e0e7bf60f3394f3abb6e6f67c63a6
Netanel <netanell125@gmail.com>
2022-06-30 15:27:09 +0300

    fake store server index

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const req = require("express/lib/request")
 
 require("dotenv").config()
 const
-express = require("express"),
-app = express(),
-PORT = process.env.PORT,
-cors = require("cors"),
-categoryRouter = require("./Router/categoryRouter"),
-productRouter = require("./Router/productRouter")
+    express = require("express"),
+    app = express(),
+    PORT = process.env.PORT || 3100,
+    cors = require("cors"),
+    categoryRouter = require("./Router/categoryRouter"),
+    productRouter = require("./Router/productRouter")
 
 require("./db")
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
 categoryRouter(app)
 productRouter(app)
 
-app.listen(PORT || 8787, ()=> console.log(`Server is running...port: ${PORT}`))
+app.listen(PORT || 8787, () => console.log(`Server is running...port: ${PORT}`))
